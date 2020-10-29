c03ddb1af7f7116c6bb0e15babd6f81fc190b40a
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-29 00:24:57 -0700

    what I know so far without having to google fu

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -4,12 +4,18 @@ var currentDay = document.querySelector("#currentDay")
 currentDay.textContent = moment().format("dddd, MMMM Do");
 
 // create row for the calendar
+var createRow = document.createElement("row")
 // create column for time
+var createColumnTime = document.createElement("timeDiv")
 // create column for entering text
+var createColumnText = document.createElement("textDiv")
 // create column for save button
+var createColumnSaveButton = document.createElement("buttonDiv")
 // append columns to row
+Row.append(createColumnTime, createColumnText, createColumnSaveButton)
 // append row to container
-
+var container = document.getElementsByClassName(".container");
+container.append(row)
 // imma need local storage to store the text ARRGGGHHH!!!
 
 // need to add colors to text column
