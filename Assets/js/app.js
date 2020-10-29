b5413f6054459b0348f0f740dd424097bce9bd7a
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-29 00:09:52 -0700

    outline logic

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,2 +1,17 @@
+//probably need to make this a function and loop it
+// set day, month, date using moment.js
 var currentDay = document.querySelector("#currentDay")
-currentDay.textContent = moment().format("dddd, MMMM Do")
+currentDay.textContent = moment().format("dddd, MMMM Do");
+
+// create row for the calendar
+// create column for time
+// create column for entering text
+// create column for save button
+// append columns to row
+// append row to container
+
+// imma need local storage to store the text ARRGGGHHH!!!
+
+// need to add colors to text column
+
+// working column save button
