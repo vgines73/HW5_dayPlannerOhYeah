b3410ff0bb9e190553b3473f6b6f86e9a8048feb
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-29 00:43:33 -0700

    added columns using bootstrap class on js and appending it

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,21 +1,27 @@
 //probably need to make this a function and loop it
 // set day, month, date using moment.js
-var currentDay = document.querySelector("#currentDay")
+var currentDay = document.querySelector("#currentDay");
 currentDay.textContent = moment().format("dddd, MMMM Do");
 
 // create row for the calendar
-var createRow = document.createElement("row")
+var createRow = document.createElement("row");
 // create column for time
-var createColumnTime = document.createElement("timeDiv")
-// create column for entering text
-var createColumnText = document.createElement("textDiv")
+var createColumnTime = document.createElement("timeDiv");
+var columnSizeTime = getElementsByClassName(".col-md-2");
+createColumnTime.append(columnSize);
+// create column for entering tex
+var createColumnText = document.createElement("textDiv");
+var columnSizeText = getElementsByClassName(".col-md-8");
+createColumnText.append(columnSizeText)
 // create column for save button
-var createColumnSaveButton = document.createElement("buttonDiv")
+var createColumnSaveButton = document.createElement("buttonDiv");
+var columnSizeSaveButton = getElementsByClassName(".col-md-2");
+createColumnSaveButton.append(columnSizeSaveButton);
 // append columns to row
-Row.append(createColumnTime, createColumnText, createColumnSaveButton)
+Row.append(createColumnTime, createColumnText, createColumnSaveButton);
 // append row to container
 var container = document.getElementsByClassName(".container");
-container.append(row)
+container.append(row);
 // imma need local storage to store the text ARRGGGHHH!!!
 
 // need to add colors to text column
