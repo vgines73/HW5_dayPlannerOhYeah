31b51ca1b3f7253d100e310fd89a41f6cd17b4c5
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-30 00:31:39 -0700

    time goes into local storage

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,12 +1,25 @@
+$(document).ready(function(){
+// current day, month, date using moment.js
+var currentDay = moment().format("dddd, MMMM, Do")
+$("#currentDay").text(currentDay)
 
-var currentDay = document.querySelector("#currentDay");
-currentDay.textContent = moment().format("dddd, MMMM, Do");
-for (let i = 9; i < 18; i++) {
-console.log()
-}
+var times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
+times.forEach(time => {
+    var timeCheck = window.localStorage.getItem(time)
+    console.log(timeCheck) // check to see if it shows, shows null
 
+    if (timeCheck === null) {
+        window.localStorage.setItem(time, "") // stores the time in local storage
+    }
+})
+
+// preventDefault for submitting
 $("form").on(("submit"), function(e) {
     e.preventDefault();
-    console.log(e.target)
+    console.log(e.target) // see if we can target the form
+    console.log(e.target.querySelector("input").getAttribute("id")) // to see if it can save into local storage
+    console.log(e.target.querySelector("input").value) // to see if it can save the text entered so we can store into local storage
+})
+
 })
