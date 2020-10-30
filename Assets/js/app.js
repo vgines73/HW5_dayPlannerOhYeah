2cbc108b7bf5324a65515e8a9fe90115e66aa8e4
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-30 00:05:38 -0700

    event.preventDefault console.log

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-    var currentDay = document.querySelector("#currentDay");
-    currentDay.textContent = moment().format("dddd, MMMM, Do");
-    for (let i = 9; i < 18; i++) {
-        console.log(currentDay)
-    }
+
+var currentDay = document.querySelector("#currentDay");
+currentDay.textContent = moment().format("dddd, MMMM, Do");
+for (let i = 9; i < 18; i++) {
+console.log()
+}
+
+
+$("form").on(("submit"), function(e) {
+    e.preventDefault();
+    console.log(e.target)
 })
